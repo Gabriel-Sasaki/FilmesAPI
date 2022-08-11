958c747e569deedd062961d8c744fdd45c19cc2d
Gabriel Sasaki <gabrielpintosasaki@outlook.com>
2022-08-11 14:07:34 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
